ded016d9c13a70d5e8c7060ca344d6be865ad23a
AndrewOch <38253789+AndrewOch@users.noreply.github.com>
2023-10-02 23:52:27 +0300

    TextField Tasks

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import {
+  StyleSheet,
+  Text,
+  View,
   FlatList,
   Platform,
   ScrollView,
@@ -14,31 +16,69 @@ import {
 const fontStyles = ["normal", "italic"];
 
 const App = () => {
-  return (
-      <InputTask1/>
-  );
-}
+  return <AuthForm />;
+};
 
 export const InputTask1 = () => {
   const [name, setName] = useState("");
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignContent: "center",
-        justifyContent: "center",
-        padding: 16,
-      }}>
-      <Text style={{ marginVertical: 16 }}>
-        {name ? `Hi ${name}!` : "What is your name?"}{" "}
+    <View style={styles.container}>
+      <Text style={{ marginVertical: 16, color: "black" }}>
+        {name ? `Hi ${name}!` : "What is your name?"}
       </Text>
       <TextInput
-        style={{ padding: 8, backgroundColor: "#f5f5f5" }}
-        onChangeText={(text) => setName(text)} />{" "}
+        style={styles.textInput}
+        onChangeText={(text) => setName(text)}
+      />
     </View>
   );
-}
+};
+
+export const AuthForm = () => {
+  const correctLogin = "username";
+  const correctPassword = "password";
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [showError, setError] = useState(false);
+  const [auth, setAuth] = useState(false);
+
+  const handlePress = () => {
+    if (login === correctLogin && password === correctPassword) {
+      setError(false);
+      setAuth(true);
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {auth ? (
+        <Text style={{ color: "black" }}>Welcome!</Text>
+      ) : (
+        <>
+          <Text style={{ color: "black" }}>Login</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Login"
+            onChangeText={(text) => setLogin(text)}
+          />
+          <Text style={{ color: "black" }}>Password</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            onChangeText={(text) => setPassword(text)}
+          />
+          <Text style={{ color: "red" }}>
+            {showError && "Incorrect login or password!"}
+          </Text>
+          <Button title="Sign in" onPress={handlePress}></Button>
+        </>
+      )}
+    </View>
+  );
+};
 
 export const Box = (props) => (
   <View
@@ -47,7 +87,7 @@ export const Box = (props) => (
       height: props.height,
       backgroundColor: props.color,
       alignSelf: props.alignSelf,
-      borderRadius: props.borderRadius
+      borderRadius: props.borderRadius,
     }}
   />
 );
@@ -55,14 +95,24 @@ export const Box = (props) => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 16,
+    backgroundColor: "#ddd",
+    width: "100%",
+    gap: 20,
   },
   text: {
     color: "green",
     fontWeight: "bold",
     fontSize: 80,
+  },
+  textInput: {
+    padding: 8,
+    backgroundColor: "#f5f5f5",
+    alignSelf: "stretch",
+    marginHorizontal: 20,
+    borderRadius: 5,
   },
 });
 
