55c1bb2a3779378a90ec03b5299e3640bfa4891b
AndrewOch <38253789+AndrewOch@users.noreply.github.com>
2023-10-02 22:54:41 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  FlatList,
+  Platform,
+  ScrollView,
+  Switch,
+  Button,
+  TouchableWithoutFeedback,
+  TextInput,
+} from "react-native";
 
-export default function App() {
+const fontStyles = ["normal", "italic"];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <InputTask1/>
+  );
+}
+
+export const InputTask1 = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignContent: "center",
+        justifyContent: "center",
+        padding: 16,
+      }}>
+      <Text style={{ marginVertical: 16 }}>
+        {name ? `Hi ${name}!` : "What is your name?"}{" "}
+      </Text>
+      <TextInput
+        style={{ padding: 8, backgroundColor: "#f5f5f5" }}
+        onChangeText={(text) => setName(text)} />{" "}
     </View>
   );
 }
 
+export const Box = (props) => (
+  <View
+    style={{
+      width: props.width,
+      height: props.height,
+      backgroundColor: props.color,
+      alignSelf: props.alignSelf,
+      borderRadius: props.borderRadius
+    }}
+  />
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "green",
+    fontWeight: "bold",
+    fontSize: 80,
   },
 });
+
+export default App;
